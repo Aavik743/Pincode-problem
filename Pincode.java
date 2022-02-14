e454a81c1ba65a19a22b6da650e28ac4ddae2521
unknown <aavik743@gmail.com>
2022-02-15 02:44:51 +0530

    added: 6 digit pincode

--- Pincode.java
+++ Pincode.java
@@ -1,9 +1,25 @@
 package com.bl.pin;
 
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
 public class Pincode {
 	
 	public static void main(String[] args) {
-		System.out.println("Welcome to the PIN problem");
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter the 6 digit PIN code: ");
+		String pin = sc.nextLine();
+		
+		boolean isPin = Pattern.matches("[0-9]{6}", pin);
+		
+		if(isPin) {
+			System.out.println("Valid Pincode");
+		}	
+		else {
+			System.out.println("Invalid Pincode");
+		}
 	}
 
 }
